5bc60b74aead08e4b72435999731ad57971fa6ef
Mischa Hildebrand <mischa.hildebrand@quickbirdstudios.com>
2022-01-14 20:41:42 +0100

    Create basic grid

--- Woerdl/ContentView.swift
+++ Woerdl/ContentView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let width = 5
+    let height = 6
+
+    private var columns: [GridItem] {
+        .init(repeating: GridItem(.flexible()), count: width)
+    }
+
+    private var squareCount: Int {
+        width * height
+    }
+
     var body: some View {
-        Text("Hello, world!")
+        LazyVGrid(columns: columns, alignment: .center, spacing: 8) {
+                ForEach(1...squareCount, id: \.self) { _ in
+                    Rectangle()
+                        .aspectRatio(1, contentMode: .fill)
+                }
+        }
             .padding()
     }
 }
