1ef7ee2fa1543704d83c5dd4ea83085eaee99d1b
Mischa Hildebrand <mischa.hildebrand@quickbirdstudios.com>
2022-01-14 20:21:28 +0100

    Initial Commit

---
+++ Woerdl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Woerdl
+//
+//  Created by Mischa Hildebrand on 14.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Woerdl/WoerdlApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoerdlApp.swift
+//  Woerdl
+//
+//  Created by Mischa Hildebrand on 14.01.22.
+//
+
+import SwiftUI
+
+@main
+struct WoerdlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
