c365c9af6f9c63d63e9d6dcc45fda61f855208d8
Mischa Hildebrand <mischa.hildebrand@quickbirdstudios.com>
2022-01-15 10:48:43 +0100

    WIP: Experimenting with text field focus

--- Woerdl/WordlView.swift
+++ Woerdl/WordlView.swift
@@ -5,11 +5,18 @@
 //  Created by Mischa Hildebrand on 14.01.22.
 //
 
+import Combine
 import SwiftUI
 
 struct WordlView: View {
 
     @ObservedObject var viewModel = WordlViewModel()
+
+    struct FocusedFieldIndex: Hashable {
+        let index: Int
+    }
+
+    @FocusState var focusedField: FocusedFieldIndex?
 
     private var columns: [GridItem] {
         .init(repeating: GridItem(.flexible()), count: viewModel.width)
@@ -21,16 +28,21 @@ struct WordlView: View {
 
     var body: some View {
         LazyVGrid(columns: columns, alignment: .center, spacing: 8) {
-                ForEach(1...squareCount, id: \.self) { index in
+                ForEach(0...squareCount - 1, id: \.self) { index in
                     ZStack() {
                         Rectangle()
                             .aspectRatio(1, contentMode: .fill)
                             .cornerRadius(4)
-                        TextField("•", text: $viewModel.letters[index - 1])
+                        TextField("•", text: $viewModel.letters[index])
                             .multilineTextAlignment(.center)
                             .font(.system(.title))
                             .foregroundColor(.white)
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .onChange(of: viewModel.letters[index]) { newValue in
+                                viewModel.next(string: newValue, at: index)
+                                focusedField = .init(index: viewModel.currentIndex)
+                            }
+                            .focused($focusedField, equals: .init(index: index))
                     }
                 }
         }
--- Woerdl/WordlViewModel.swift
+++ Woerdl/WordlViewModel.swift
@@ -13,10 +13,62 @@ class WordlViewModel: ObservableObject {
     let width = 5
     let height = 6
 
-    @Published var letters: [String] = []
+    @Published var currentIndex: Int = 0
+    @Published var letters: [String] = [] {
+        didSet {
+            print(letters)
+//            print(letters)
+//            if let changedIndex = indexOfFirstNonEqualItem(oldArray: oldValue, newArray: letters) {
+//                currentIndex = changedIndex + 1
+//            }
+//            var multipleCharactersIndex: Int?
+//            for (index, letter) in letters.enumerated() {
+//                if letter.count > 1 {
+//                    multipleCharactersIndex = index
+//                }
+//            }
+//            if let index = multipleCharactersIndex {
+//                let letterCount = letters[index].count
+//                let multipleCharacters = letters[index]
+//                for letterIndex in 0...letterCount - 1 {
+//                    let characterIndex = multipleCharacters.index(multipleCharacters.startIndex, offsetBy: letterIndex)
+//                    let character = multipleCharacters[characterIndex]
+//                    letters[index + letterIndex] = String(character)
+//                }
+//            }
+        }
+    }
 
     init() {
-        letters = .init(repeating: "", count: width * height)
+        letters = .init(repeating: .init(""), count: width * height)
     }
+
+    func next(string: String?, at index: Int) {
+//        print("--- nextString:", string!, "atIndex:", index)
+        guard var string = string, !string.isEmpty else {
+            currentIndex = index
+            return
+        }
+        if index >= letters.endIndex {
+            currentIndex = 0
+        } else {
+            if string.count > 1 {
+                let firstCharacter = string.removeFirst()
+                letters[index] = String(firstCharacter)
+                next(string: string, at: index + 1)
+            } else {
+                currentIndex = index + 1
+            }
+        }
+    }
+
+//    private func indexOfFirstNonEqualItem<T: Equatable>(oldArray: Array<T>, newArray: Array<T>) -> Int? {
+//        for (index, (old, new)) in zip(oldArray, newArray).enumerated() {
+//            if old != new {
+//                return index
+//            }
+//        }
+//        return nil
+//    }
 
 }
